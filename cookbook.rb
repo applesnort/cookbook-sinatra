a5cc2263c8a7e7dc1aedc0d68544f1da09977e8c
applesnort <jmangin@gmail.com>
2020-05-08 01:30:45 +0800

    refactored save; fixed destroy method bug

--- cookbook.rb
+++ cookbook.rb
@@ -6,7 +6,8 @@ class Cookbook
   def initialize(csv_file_path)
     @recipes = []
     @csv_file_path = csv_file_path
-    import_csv
+    @next_id = 1
+    import_csv if File.exist?(@csv_file_path)
   end
 
   attr_reader :recipes
@@ -16,7 +17,9 @@ class Cookbook
   end
 
   def add_recipe(new_recipe)
+    new_recipe.id = @next_id
     @recipes << new_recipe
+    @next_id += 1
     store_in_csv
   end
   
@@ -30,8 +33,8 @@ class Cookbook
     return recipe
   end
 
-  def destroy(recipe)
-    @recipes = @recipes.each.reject { |r| r.id == recipe }
+  def destroy(id)
+    @recipes = @recipes.each.reject { |item| item.id == id.to_i }
     store_in_csv
   end
 
@@ -39,18 +42,28 @@ class Cookbook
 
   def import_csv
     CSV.foreach(@csv_file_path, headers: :first_row, header_converters: :symbol) do |row|
+      row[:id] = row[:id].to_i
+      row[:name] = row[:name]
+      row[:description] = row[:description]
+      row[:prep_time] = row[:prep_time]
+      row[:difficulty] = row[:difficulty]
+      row[:done] = row[:done]
+      row[:img] = row[:img]
       @recipes << Recipe.new(row)
     end
+    @next_id = @recipes.last.id + 1 unless @recipes.empty?
   end
 
   def store_in_csv
-    column_header = ["id", "name", "description", "prep time", "difficulty", "done", "image"]
-    csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
-    CSV.open(@csv_file_path, 'wb', csv_options) do |csv|
-      csv << column_header
+    # column_header = ["id", "name", "description", "prep time", "difficulty", "done", "img"]
+    # csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+    CSV.open(@csv_file_path, 'wb') do |csv|
+      csv << %w[id name description prep_time difficulty done img]
       @recipes.each do |recipe|
         # write one row contianing a recipe object
-        csv << [recipe.id,
+        p "HELLO FROM: store_in_csv #{recipe}"
+        csv << [
+        recipe.id,
         recipe.name, 
         recipe.description, 
         recipe.prep_time, 
