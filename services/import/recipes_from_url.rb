8b8550231561fa1873aeed882429bfe0330a2b7c
applesnort <jmangin@gmail.com>
2020-05-08 01:35:16 +0800

    ensured proper order for writing to csv

--- services/import/recipes_from_url.rb
+++ services/import/recipes_from_url.rb
@@ -9,18 +9,23 @@ class RecipesFromUrl
     html_file = open(url).read
     html_doc = Nokogiri::HTML(html_file)
     output = []
+    # div.view-content article#node-99422.node.node-recipe.node-teaser-item.clearfix h3.teaser-item__title a
     html_doc.search('.view-content article').each do |element|
       name = element.search('a').text.strip
       description = element.search('div.field-item.even').text.strip
-      prep_time = element.search('li.teaser-item__info-item.teaser-item__info-item--total-time').text.strip
-      img = element.search('div.teaser-item__image a img').attr('src').to_s
+      # li.teaser-item__info-item.teaser-item__info-item--total-time span.mins
+      prep_time = element.search('li.teaser-item__info-item.teaser-item__info-item--total-time span.mins').text.strip
+      # .teaser-item__image a img
+      img = element.search('.teaser-item__image a img').attr('src').to_s
+      p img
       img = "http:" + img
-      output << Recipe.new(Hash.new(
+      output << Recipe.new(
         name: name,
         description: description,
         prep_time: prep_time,
-        img: img))
+        img: img)
     end
+    p output
     return output
   end
 end
